44a49a589107c998a1e9efbb0a0d431b942e0e01
zedaAb <zemenuabebaw402@gmail.com>
2026-04-05 00:49:51 -0700

    Update frontend API to use deployed backend URL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
+const api = axios.create({ baseURL: 'https://hackaton-rtbb.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
