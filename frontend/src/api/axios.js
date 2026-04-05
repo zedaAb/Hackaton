d96bdac9f877a0bf8b3d7225364065df47e614ed
zedaAb <zemenuabebaw402@gmail.com>
2026-04-04 17:12:56 -0700

    Some conflicing problem solved

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5001/api' });
+const api = axios.create({ baseURL: 'http://localhost:5000/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
